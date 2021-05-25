3f511d88851ab8e608a53d33aeef43786312f5f5
Alonso Gutelius <alonso_gutelius@usmp.pe>
2021-05-25 18:16:06 -0500

    Añadiendo modelo del producto

---
+++ Models/Producto.cs
@@ -0,0 +1,19 @@
+namespace PC3.Models
+{
+    public class Producto
+    {
+        public int Id {get; set; }
+        public string NomProducto {get;set; }
+        public string Foto {get;set; }
+        public string  Descripcion {get; set;}
+        public string  Precio {get;set;}
+        public string Nombre {get;set;}
+        public string Celular {get;set;}
+       
+        public Categoria Categoria {get;set;}
+
+        public int CategoriaId {get;set; }
+    }
+
+    
+}
